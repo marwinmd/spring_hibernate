831956f62ba380058b5b329d09152a40f96f2df1
Marwin Wiechert <marwin.wiechert@opitz-consulting.com>
2016-10-18 14:02:16 +0200

    Java Annotations

---
+++ spring-demo-annotations/src/com/wiechert/springdemo/HappyFortuneService.java
@@ -0,0 +1,13 @@
+package com.wiechert.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return "You are lucky as fuck today";
+	}
+
+}
